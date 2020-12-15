f13909062d24c2dba17092d34e62a8d0ddf6956c
Mogeko <zhengjunyi@live.com>
2020-12-15 23:14:39 +0100

    feat(Mdx): Added support for PlantUML code

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,6 +25,7 @@ module.exports = {
       resolve: `gatsby-plugin-mdx`,
       options: {
         extensions: [`.mdx`, `.md`],
+        gatsbyRemarkPlugins: [`gatsby-remark-plantuml-lite`],
       },
     },
     `gatsby-transformer-sharp`,
