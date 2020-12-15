66c7529a126a79bcb214d59b52ef63b92935d395
Mogeko <zhengjunyi@live.com>
2020-12-15 18:04:56 +0100

    feat(404): Redesign the 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,47 @@
 import React from "react"
-
+import { styled } from "linaria/react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+export default function NotFoundPage() {
+  const emojis = [
+    "\\(o_o)/",
+    "(o^^)o",
+    "(˚Δ˚)b",
+    "(^-^*)",
+    "(≥o≤)",
+    "(^_^)b",
+    "(·_·)",
+    "(='X'=)",
+    "(>_<)",
+    "(;-;)",
+    "\\(^Д^)/",
+    "_(:з」∠)_",
+  ]
 
-export default NotFoundPage
+  return (
+    <Layout>
+      <SEO title="404: Not found" />
+      <NotFound>
+        <h1>{emojis[Math.floor(Math.random() * emojis.length)]}</h1>
+        <p>/* 404 page not found. */</p>
+        <p>
+          <a
+            href="#"
+            onClick={() => {
+              window.history.back()
+            }}
+          >
+            ↑ Go Back ↑
+          </a>
+        </p>
+      </NotFound>
+    </Layout>
+  )
+}
+
+const NotFound = styled.div`
+  font-size: 3em;
+  transform: translateY(30vh);
+  text-align: center;
+`
