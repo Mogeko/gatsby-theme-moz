821c7cfaff6f2550a6c22e82017bf51f6c6ff6f9
Mogeko <zhengjunyi@live.com>
2020-12-19 23:11:55 +0100

    refactor(Footer): Adjust code structure

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,55 +1,81 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faHeart } from "@fortawesome/free-solid-svg-icons"
-import { styled } from "linaria/react"
+import { Link } from "gatsby"
+import { css } from "linaria"
 import PropTypes from "prop-types"
 import React from "react"
 
 export default function Footer({ copyrightYear, author }) {
-  const nowYear = new Date().getFullYear()
-
+  const style = css`
+    width: 100%;
+    text-align: center;
+    line-height: 4rem;
+    margin-top: 2em;
+    div {
+      font-size: 0.95em;
+      span {
+        margin: 0 0.123em;
+      }
+    }
+  `
   return (
-    <FooterBar>
-      <Copyright>
-        <span>&copy;</span>
-        <span>
-          {copyrightYear === nowYear
-            ? `${copyrightYear}`
-            : `${copyrightYear} - ${nowYear}`}
-        </span>
-        <span>
-          <FontAwesomeIcon icon={faHeart} size="sm" />
-        </span>
-        <span>
-          <a href="/about">{author}</a>
-        </span>
+    <footer className={style}>
+      <div>
+        <Copyright copyrightYear={copyrightYear} author={author} />
         <span>|</span>
-        <span>
-          Powered by <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </span>
-      </Copyright>
-    </FooterBar>
+        <Engine />
+      </div>
+    </footer>
   )
 }
 
-const FooterBar = styled.footer`
-  width: 100%;
-  text-align: center;
-  line-height: 4rem;
-  margin-top: 2em;
-`
+const Copyright = ({ copyrightYear, author }) => {
+  const nowYear = new Date().getFullYear()
+  return (
+    <>
+      <span>&copy;</span>
+      <span>
+        {copyrightYear === nowYear
+          ? `${copyrightYear}`
+          : `${copyrightYear} - ${nowYear}`}
+      </span>
+      <span>
+        <FontAwesomeIcon icon={faHeart} size="sm" />
+      </span>
+      <span>
+        <Link href="/about">{author}</Link>
+      </span>
+    </>
+  )
+}
 
-const Copyright = styled.div`
-  font-size: 0.95em;
-  span {
-    margin: 0 0.123em;
-  }
-`
+const Engine = () => {
+  const style = css`
+    a:hover {
+      color: #663399;
+    }
+  `
+  return (
+    <span className={style}>
+      Powered by <Link to="https://www.gatsbyjs.com">Gatsby</Link>
+    </span>
+  )
+}
 
-Footer.propTypes = {
+Copyright.propTypes = {
   author: PropTypes.string.isRequired,
   copyrightYear: PropTypes.number,
 }
 
-Footer.defaultProps = {
+Copyright.defaultProps = {
   copyrightYear: 2020,
+  author: ``,
+}
+
+Footer.propTypes = {
+  ...Copyright.propTypes,
+}
+
+Footer.defaultProps = {
+  ...Copyright.defaultProps,
 }
