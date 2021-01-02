e96af03cda682a5145ba885b11109574e1a32ea6
Mogeko <zhengjunyi@live.com>
2021-01-02 19:04:51 +0100

    fix(Posts): Redesign the style of Posts components

--- src/components/mdx/components.js
+++ src/components/mdx/components.js
@@ -7,6 +7,7 @@ import {
   lightPostLinkColor,
   lightPostLinkHoverColor,
   lightCodeNotclassBackgroundColor,
+  lightGlobalLinkColor,
 } from "../../styles/variables"
 import { css, cx } from "linaria"
 import { Link } from "gatsby"
@@ -14,27 +15,19 @@ import { Link } from "gatsby"
 export const Content = styled.div`
   margin-top: 2rem;
   p {
-    font-size: 1em;
-    margin: 0.5em 0;
-  }
-  h1 {
-    font-size: 2em;
-    margin: 0.67em 0;
-  }
-  h2,
-  h3,
-  h4,
-  h5,
-  h6 {
-    margin: 0.8em auto 0.3em;
+    font-size: 1rem;
+    line-height: 1.7;
+    margin: 0 0 1.75rem;
+    padding: 0;
   }
   ol,
   ul {
-    padding-left: 1em;
-  }
-  & > ol,
-  & > ul {
-    padding-left: 2em;
+    margin: 0 0 0.5rem 2rem;
+    padding: 0;
+    ol,
+    ul {
+      margin-left: 1rem;
+    }
   }
   blockquote {
     font: italic 14px/22px normal helvetica, serif, STSong;
@@ -49,7 +42,7 @@ export const Content = styled.div`
   }
   del {
     text-decoration: line-through red;
-    text-decoration-thickness: 0.125em;
+    text-decoration-thickness: 0.125rem;
   }
   a {
     color: ${lightPostLinkColor};
@@ -72,29 +65,32 @@ const useAnchor = (WrappedComponent, { children }) => {
   const anchorLink = children
     .replace(/(\s+)|([.,!?:;'\"\'-])/g, "-")
     .toLowerCase()
-  const Beacon = css`
-    position: relative;
-    .hide {
-      display: none;
+  const Component = css`
+    .anchor {
+      position: relative;
+      .hide {
+        display: none;
+      }
+      .anchor-link {
+        position: absolute;
+        top: 50%;
+        left: 0.75rem;
+        right: -2rem;
+        color: ${lightGlobalLinkColor};
+        transform: translateY(-50%);
+      }
     }
-  `
-  const Anchor = css`
-    position: absolute;
-    top: 50%;
-    left: 0.75rem;
-    transform: translateY(-50%);
-    right: -2rem;
-    color: #161209;
   `
   return (
     <WrappedComponent
       id={anchorLink}
+      className={Component}
       onMouseOver={handleMouseOver}
       onMouseOut={handleMouseOut}
     >
       {children}
-      <span className={Beacon}>
-        <Link className={cx(Anchor, active)} to={`#${anchorLink}`}>
+      <span className="anchor">
+        <Link className={cx("anchor-link", active)} to={`#${anchorLink}`}>
           <FontAwesomeIcon icon={faLink} size="xs" flip="horizontal" />
         </Link>
       </span>
@@ -102,11 +98,22 @@ const useAnchor = (WrappedComponent, { children }) => {
   )
 }
 
-const H1Anchor = props => useAnchor(`h1`, props)
+const H1Anchor = props =>
+  useAnchor(
+    styled.h1`
+      font-size: 2.5rem;
+      line-height: 1.25;
+      margin: 3.5rem 0 1.75rem;
+    `,
+    props
+  )
 
 const H2Anchor = props =>
   useAnchor(
     styled.h2`
+      font-size: 1.8rem;
+      line-height: 1.25;
+      margin: 2.8rem 0 1.25rem;
       &::before {
         content: "#";
         margin-right: 5px;
@@ -119,6 +126,10 @@ const H2Anchor = props =>
 const H3Anchor = props =>
   useAnchor(
     styled.h3`
+      font-size: 2.2rem;
+      line-height: 1.25;
+      margin: 3.2rem 0 1.5rem;
+
       &::before {
         content: "|";
         margin-right: 5px;
@@ -128,10 +139,30 @@ const H3Anchor = props =>
     props
   )
 
-const H4Anchor = props => useAnchor(`h4`, props)
+const H4Anchor = props =>
+  useAnchor(
+    styled.h4`
+      font-size: 1.5rem;
+      line-height: 1.25;
+      margin: 2.4rem 0 1rem;
+    `,
+    props
+  )
+
+const H5 = styled.h5`
+  font-size: 1.2rem;
+  line-height: 1.25;
+  margin: 2rem 0 0.8rem;
+`
+
+const H6 = styled.h6`
+  font-size: 1rem;
+  line-height: 1.25;
+  margin: 1.5rem 0 0.5rem;
+`
 
 const Table = props => {
-  const style = css`
+  const Table = css`
     width: 90%;
     margin: 10px auto;
     overflow-x: auto;
@@ -161,7 +192,7 @@ const Table = props => {
     }
   `
   return (
-    <div className={style}>
+    <div className={Table}>
       <table {...props} />
     </div>
   )
@@ -172,13 +203,13 @@ const InlineCode = styled.code`
   margin: 0 2px;
   background: ${lightCodeNotclassBackgroundColor};
   border-radius: 5px;
-  font-size: 0.9em;
+  font-size: 0.9rem;
 `
 
 const Image = props => {
   const style = css`
     display: block;
-    min-width: 90%;
+    min-width: calc(100% * 2 / 3);
     max-width: 100%;
     height: auto;
     margin: 0 auto;
@@ -192,6 +223,8 @@ const components = {
   h2: H2Anchor,
   h3: H3Anchor,
   h4: H4Anchor,
+  h5: H5,
+  h6: H6,
   table: Table,
   inlineCode: InlineCode,
   img: Image,
